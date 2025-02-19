1a7a7bb3bd10a7fbbc9b5331bab941ed6aabd9a9
vgiorgiecdchdh <giogio35v@gmail.com>
2025-02-19 20:40:43 +0400

    first commit

---
+++ day 01/homework/hw.py
@@ -0,0 +1,30 @@
+name = "giorgi"
+# name არის ცვლადი
+# არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "giorgi" არის ცვლადისთვის მნიშვნელობა
+surname = "mushkudiani"
+
+
+# print(name)
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი 
+
+name = "giorgi" #ეს არის str (string) ტიპის ცვლადი
+age = 22 #ეს არის int (integer) მთელი რიცხვი 
+height = 184.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი 
+
+knows_programming = True #true ან false 
+is_ugly = False #snarecase(უბრალო წერის სტილი სტარდანტულად)
+
+isuglay = False # ჯავასკრიპტული cameLcase
+
+print(name + " " + surname)
+
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბლოები
+print(name + age)
+
+print(type (age))
+print(type (name))
+print(type (surname))
+print(type (height))
