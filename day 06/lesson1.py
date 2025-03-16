a5de885b0378b84e003476184b633133bab958bf
vgiorgiecdchdh <giogio35v@gmail.com>
2025-03-16 15:58:42 +0400

    second commit

---
+++ day 06/lesson1.py
@@ -0,0 +1,15 @@
+name_of_person=("goa best")
+name_giorgi_classwork=("goas classwork")
+goa_classwork_naic=("group59")
+elevel_06_classwork=("this is python lesson")
+goa_level_06=("classwork goa best")
+goa_goa_goa=("finish")
+
+
+nameperson="giorgi"
+goalesson="good classwork"
+ilikegoa="goa is good academy"
+
+
+name=input("enter your name: ")
+name=input("Enter your friends name: ")
---
+++ day 06/lesson11.py
@@ -0,0 +1,12 @@
+Name_homework_goa=("goa best")
+NAMEisspidermane=("academy goa")
+
+
+Name=input("Enter your food: ")
+print("Name=input")
+namE=input("enter your age: ")
+nAme=input("enter your frends age: ")
+(11+10)
+(11-10)
+(11*10)
+(11/10)
